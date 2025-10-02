381693f41bda89df073ea089753955f59e0f119e
wesam-sakr <wesamsakr801@gmail.com>
2025-10-02 17:06:32 +0300

    training page

--- Emaan training Website/js/script.js
+++ Emaan training Website/js/script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
       992: {
         items: 5,
       },
-      1200:{
+      1200: {
         items: 6,
         mouseDrag: itemsCount > 6, // السماح بالسحب فقط لو >=6 
         touchDrag: itemsCount > 6
       }
     }
   });
-  
+
   var $externalCarousel = $(".external-programs .owl-carousel");
   var externalItemsCount = $externalCarousel.find(".card").length; // عدد العناصر
   $(".external-programs .owl-carousel").owlCarousel({
@@ -66,7 +66,7 @@ $(document).ready(function () {
         stagePadding: 40,
         items: 4
       },
-      1200:{
+      1200: {
         items: 4,
         mouseDrag: externalItemsCount > 4, // السماح بالسحب فقط لو >=4 
         touchDrag: externalItemsCount > 4
@@ -74,42 +74,30 @@ $(document).ready(function () {
     }
   });
 
-  $(".program-slider .owl-carousel").owlCarousel({
-    nav: false,
-    loop: false,
-    dots: true,
-    responsiveClass: true,
-    margin: 60,
-    rtl: dirAr,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      992: {
-        items: 2,
-      },
-      1200:{
-        items: 3,
+  $(".owl-carousel").each(function () {
+    let isExternal = $(this).closest(".program-slider").hasClass("external");
+    $(this).owlCarousel({
+      nav: true,
+      loop: false,
+      dots: false,
+      responsiveClass: true,
+      margin: 20,
+      rtl: dirAr,
+      navText: [
+    '<i class="fa-solid fa-chevron-right"></i>', // سهم يمين
+    '<i class="fa-solid fa-chevron-left"></i>'  // سهم شمال
+  ],
+      responsive: isExternal ? {
+        0: { items: 1 },
+        992: { items: 2 }
+      } : {
+        0: { items: 1 },
+        756: { items: 2 },
+        1200: { items: 3 }
       }
-    }
+    });
   });
 
-  $(".external.program-slider .owl-carousel").owlCarousel({
-    nav: false,
-    loop: false,
-    dots: true,
-    responsiveClass: true,
-    margin: 60,
-    rtl: dirAr,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      992: {
-        items: 2,
-      }
-    }
-  });
 
   AOS.init({
     duration: 1000
